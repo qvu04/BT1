24b4503e20ec7f157478fb59873f487a9ae9fb09
quangvungo <quangvu221004@gmail.com>
2024-11-27 07:52:32 +0700

    khoi tao du an

---
+++ index.js
@@ -0,0 +1,44 @@
+//BT1
+// dau vao
+let luongMotNgay = 100000, soNgayLam = 5, tinhLuong;
+
+// cac buoc thuc hien
+tinhLuong = luongMotNgay * soNgayLam;
+//dau ra
+console.log("Lương Nhân Viên trong 5 ngày: " + tinhLuong);
+
+//BT2
+// dau vao
+let a = 1, b = 2, c = 3, d = 4, e = 5, giaTriTrungBinh;
+//cac buoc thuc hien
+giaTriTrungBinh = (a + b + c + d + e) / 5;
+//dau ra
+console.log("Giá trị trung bình là: " + giaTriTrungBinh);
+
+//BT3
+// dau vao
+let USD = 2, tienViet = 23500, doiTien;
+//cac buoc thuc hien
+doiTien = USD * tienViet;
+//dau ra
+console.log("2 USD: " + doiTien);
+
+//BT4
+//dau vao
+let chieuDai = 10, chieuRong = 5, chuVi, dienTich;
+//cac buoc thuc hien
+chuVi = (chieuDai + chieuRong) * 2;
+dienTich = chieuDai * chieuRong;
+//dau ra
+console.log("Chu Vi bằng: " + chuVi);
+console.log("Diện tích bằng: " + dienTich);
+
+//BT5
+//dau vao
+let soHaiChuSo = 88, soHangDonVi, soHangChuc, tinhTong;
+//cac buoc thuc hien
+soHangDonVi = 8 % 10;
+soHangChuc = 80 / 10;
+tinhTong = soHangDonVi + soHangChuc;
+//dau ra
+console.log("Tổng là: " + tinhTong);
