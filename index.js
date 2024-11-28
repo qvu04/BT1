f15ddc9ce3f55a393f4f730f41341f8e20026387
qvu04 <quangvu221004@gmail.com>
2024-11-29 00:16:15 +0700

    update all

--- index.js
+++ index.js
@@ -1,46 +1,94 @@
 //BT1
-// dau vao
-let luongMotNgay = 100000, soNgayLam = 5, tinhLuong;
+// đầu vào
+let luongMotNgay = document.querySelector("#luongMotNgay");
+let soNgayLam = document.querySelector("#soNgayLam");
+let result_1 = document.querySelector("#result_1");
+let tinhLuong = document.querySelector("#tinhLuong");
+luongMotNgay.value = 1000;
+// các bước thực hiện
+tinhLuong = addEventListener("click", function () {
+    let luongMotNgayValue = luongMotNgay.value;
+    let soNgayLamValue = soNgayLam.value;
+    let message = luongMotNgayValue * soNgayLamValue;
+// đầu ra
+    result_1.innerHTML = `Kết quả là: ${message}`;
+});
 
-// cac buoc thuc hien
-tinhLuong = luongMotNgay * soNgayLam;
-//dau ra
-console.log("Lương Nhân Viên trong 5 ngày: " + tinhLuong);
-
-//BT2
-// dau vao
-let a = 1, b = 2, c = 3, d = 4, e = 5, giaTriTrungBinh;
-//cac buoc thuc hien
-giaTriTrungBinh = (a + b + c + d + e) / 5;
-//dau ra
-console.log("Giá trị trung bình là: " + giaTriTrungBinh);
+// BT2
+// đầu vào
+let soa = document.querySelector("#soa");
+let sob = document.querySelector("#sob");
+let soc = document.querySelector("#soc");
+let sod = document.querySelector("#sod");
+let soe = document.querySelector("#soe");
+let tinhTrungBinh = document.querySelector("#tinhTrungBinh");
+let result_2 = document.querySelector("#result_2")
+// các bước thực hiện
+tinhTrungBinh = addEventListener("click", function () {
+        let soaValue = soa.value;
+        let sobValue = sob.value;
+        let socValue = soc.value;
+        let sodValue = sod.value;
+        let soeValue = soe.value;
+        let message = (soaValue/5) + (sobValue/5) + (socValue/5) + (sodValue/5) + (soeValue/5);
+// đầu ra
+        result_2.innerHTML = `Kết quả là: ${message}`;
+}
+);
 
 //BT3
-// dau vao
-let USD = 2, tienViet = 23500, doiTien;
-//cac buoc thuc hien
-doiTien = USD * tienViet;
-//dau ra
-console.log("2 USD: " + doiTien);
+// đầu vào
+let usd = document.querySelector("#USD");
+let tienViet = document.querySelector("#tienViet");
+let tinhTien = document.querySelector("#tinhTien");
+let result_3 = document.querySelector("#result_3");
+usd.value = 23500;
+// các bước thực hiện
+tinhTien = addEventListener("click", function () {
+    let usdValue = usd.value;
+    let tienVietValue = tienViet.value;
+    let message = usdValue * tienVietValue;
+// đầu ra
+    result_3.innerHTML = `Kết quả là: ${message}`;
+});
 
 //BT4
-//dau vao
-let chieuDai = 10, chieuRong = 5, chuVi, dienTich;
-//cac buoc thuc hien
-chuVi = (chieuDai + chieuRong) * 2;
-dienTich = chieuDai * chieuRong;
-//dau ra
-console.log("Chu Vi bằng: " + chuVi);
-console.log("Diện tích bằng: " + dienTich);
+// đầu vào
+let chieuDai = document.querySelector("#chieuDai");
+let chieuRong = document.querySelector("#chieuRong");
+let tinh = document.querySelector("#tinh");
+let chuVi = document.querySelector("#chuVi");
+let dienTich = document.querySelector("#dienTich");
+// các bước thực hiện
+tinh = addEventListener("click", function () {
+    let chieuDaiValue = chieuDai.value;
+    let chieuRongValue = chieuRong.value;
+    let message_1 = (chieuDaiValue*2) + (chieuRongValue*2);
+    let message_2 = chieuDaiValue * chieuRongValue;
+// đầu ra
+    chuVi.innerHTML = `Chu vi là: ${message_1}`;
+    dienTich.innerHTML = `Diện Tích là: ${message_2}`;
+})
 
 //BT5
-//dau vao
-let soHaiChuSo = 88, soHangDonVi, soHangChuc, tinhTong;
-//cac buoc thuc hien
-soHangDonVi = 8 % 10;
-soHangChuc = 80 / 10;
-tinhTong = soHangDonVi + soHangChuc;
-//dau ra
-console.log("Tổng là: " + tinhTong);
+// đầu vào
+let soHaiChuSo = document.querySelector("#soHaiChuSo");
+let tinhTong = document.querySelector("#tinhTong");
+let result_4 = document.querySelector("#result_4");
+let a,b;
+// các bước thực hiện
+tinhTong = addEventListener("click", function () {
+    let soHaiChuSoValue = soHaiChuSo.value;
+    a = soHaiChuSoValue % 10;
+    b = Math.floor(soHaiChuSoValue/10);
+    let message = a + b;
+    result_4.innerHTML = `Tổng của số Hai Chữ Số là: ${message}`;
 
-adadsasdasd
+})
+
+
+
+
+
+
+
